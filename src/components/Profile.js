b7ca9fc33f1a69e878805f32ac0707a6a9a8d401
hussain <hussainibnthaj@gmail.com>
2019-02-25 17:27:38 +0530

    Adds user snippet styles.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,9 @@ const Profile = props => {
   const user = JSON.parse(localStorage.getItem('AUTH_TOKEN'));
   return (
     <div className="container">
+      <div className="user-snippet">
+        <h1>HT</h1>
+      </div>
       <h2>{user.user_display_name}</h2>
       <button onClick={() => {
         localStorage.removeItem('AUTH_TOKEN');
